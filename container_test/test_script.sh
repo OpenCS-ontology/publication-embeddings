c376c0eb247c635ef26b7b77cc2a890a91fba881
grzegorzZ1 <lukgrzes@gazeta.pl>
2023-11-25 19:24:09 +0100

    Test upgrade

--- container_test/test_script.sh
+++ container_test/test_script.sh
@@ -1,4 +1,5 @@
 mkdir -p /home/input_ttl_files/csis/volume_test
+mkdir -p /home/input_ttl_files/scpe/volume_test
 cp container_test/ttl_input/* /home/input_ttl_files/csis/volume_test
 python3 /home/embed_papers.py
 mkdir /container_test/created_ttl
